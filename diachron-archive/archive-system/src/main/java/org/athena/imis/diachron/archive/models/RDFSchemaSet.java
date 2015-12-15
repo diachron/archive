0f06cb5a24b13a29f1e0a9c949b269594440c025
Marios Meimaris <m.meimaris@gmail.com>
2015-12-15 15:24:57 +0200

    Added SchemaSet interface and model for RDF

---
+++ diachron-archive/archive-system/src/main/java/org/athena/imis/diachron/archive/models/RDFSchemaSet.java
@@ -0,0 +1,5 @@
+package org.athena.imis.diachron.archive.models;
+
+public class RDFSchemaSet extends AbstractDiachronEntity  implements SchemaSet {
+
+}
---
+++ diachron-archive/archive-system/src/main/java/org/athena/imis/diachron/archive/models/SchemaSet.java
@@ -0,0 +1,7 @@
+package org.athena.imis.diachron.archive.models;
+
+
+public interface SchemaSet extends DiachronEntity {
+	
+	
+}
